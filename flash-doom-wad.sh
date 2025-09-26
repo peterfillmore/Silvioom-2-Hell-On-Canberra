37b47ddd6a5b2cbe217e6587009ddb443fb4bc73
Peter Fillmore <peter@peterfillmore.com>
2025-09-26 17:48:33 +1000

    ROM stuffing

--- flash-doom-wad.sh
+++ flash-doom-wad.sh
@@ -1,6 +1,6 @@
 #esptool.py --port /dev/ttyACM0  --baud $((115200*4)) write_flash 0x210000 doom1gba.wad
 #esptool.py --port /dev/ttyACM0  --baud $((115200*4)) write_flash 0x314000 doom1gba.wad
-esptool.py --port /dev/ttyACM0  --baud $((115200*4)) write_flash 0x1d6000 silviodoom.wad
+esptool.py --port /dev/ttyACM0  --baud $((115200*4)) write_flash 0x1a4000 silviodoom.wad
 #esptool.py --port /dev/ttyACM0  --baud $((115200*4)) write_flash 0x210000 gbadoomcompressed.wad 
 #esptool.py --port /dev/ttyACM0  --baud $((115200*4)) write_flash 0x210000 gbadoom1.wad
 #esptool.py --port /dev/ttyACM0  --baud $((115200*4)) write_flash 0x210000 doom1.wad
